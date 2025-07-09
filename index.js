d44513f429d13091bb9134179003556bce698400
belem-cell <belemzenil@gmail.com>
2025-07-08 22:47:34 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-50">
+      <h1 className="text-3xl font-bold text-blue-800">Cobro Agua App funcionando 🚰✅</h1>
+    </div>
+  );
+}
